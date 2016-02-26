c805a3fefd2ba3511300d4c7804dd05e854fa6e6
bubkoo <bubkoo@163.com>
2016-02-26 18:30:52 +0800

    temp

--- bin/app.js
+++ bin/app.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 
+var colors = require('colors');
 var pkg = require('../package.json');
 var console = require('../lib/console');
+var startServer = require('../lib');
 
 process.title = pkg.name;
 
 var options = console.parse(process.argv);
 
-console.printLogo('magenta');
-
-require('../lib')(options);
+startServer(options);
